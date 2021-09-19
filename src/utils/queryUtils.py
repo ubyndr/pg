4e6468763860e4250dae10288360021c9c3b3ba1
Ismail Ugur Bayindir <ubyndr@gmail.com>
2021-09-19 19:04:07 +0300

    Update queryUtils.py

--- src/utils/queryUtils.py
+++ src/utils/queryUtils.py
@@ -1,9 +1,9 @@
 def region_list_query():
-    return "SELECT DISTINCT ?s { ?s a :region . }"
+    return 'SELECT DISTINCT ?s { ?s a :region . }'
 
 
 def varietal_list_query():
-    return "SELECT DISTINCT ?s { ?s rdfs:subClassOf :varietal . }"
+    return 'SELECT DISTINCT ?s { ?s rdfs:subClassOf :varietal . }'
 
 
 def wine_list_query():
@@ -12,9 +12,9 @@ def wine_list_query():
 
 
 def wine_query(colour, varietal, region):
-    c = ":" + colour if colour else "?colour"
-    v = ":" + varietal if varietal else "?varietal"
-    r = ":" + region if region else "?region"
+    c = ':' + colour if colour else '?colour'
+    v = ':' + varietal if varietal else '?varietal'
+    r = ':' + region if region else '?region'
     return """SELECT DISTINCT ?s {
            ?s rdfs:subClassOf ?cnode. ?cnode owl:onProperty :has_color. ?cnode owl:someValuesFrom %s.
            ?s rdfs:subClassOf ?vnode. ?vnode owl:onProperty :made_from. ?vnode owl:someValuesFrom %s.
